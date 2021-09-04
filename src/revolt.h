5c80a6901b18df6ba9918e46e6acbeb827808b26
antropez <spaceraiders@tutanota.com>
2021-09-04 14:02:25 +0000

    docs: added documentation for functions

--- src/revolt.h
+++ src/revolt.h
@@ -36,7 +36,33 @@ struct RevoltUserRelation {
     char* status;
 };
 
+/*
+ * Sends a message to the private message, channel, or group with
+ * the corresponding id.
+ *
+ * @param client: the Revolt client
+ * @param message: the message to send
+ * @param target: id of the location to send the message to
+ * @return: HTTP status code
+*/
 int revoltSendMessage(struct RevoltClient* client, const char* message, const char* target);
+
+/*
+ * Retrieves information about a user with the provided id, and
+ * writes it to a buffer.
+ *
+ * @param client: the Revolt client
+ * @param target: the id of the user to use
+ * @param buffer: the buffer to write the user information to
+ * @return: HTTP status code
+*/
 int revoltGetUserInfo(struct RevoltClient* client, const char* target, struct RevoltUserInfo* buffer);
+
+/*
+ * Frees the structure containing user information.
+ *
+ * @param buffer: the buffer to free
+*/
+void revoltFreeUserInfo(struct RevoltAvatarInfo* buffer);
 
 #endif
