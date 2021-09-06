35c487ca91ce436c384f65f714be7a2ecb44c31c
antropez <spaceraiders@tutanota.com>
2021-09-06 12:59:27 +0000

    fix: ulids are generated for message sending

--- src/messaging/sendMessage.c
+++ src/messaging/sendMessage.c
@@ -3,10 +3,17 @@
 */
 
 #include "../deps/revolt.h"
+#include "../deps/ulid/ulid.h"
 #include "../deps/json-utils/utils.h"
 
 int revoltSendMessage(struct RevoltClient* client, const char* message, const char* target) {
-    char* postJSON = mprintf("{\"content\": \"%s\", \"nonce\": \"2321312312312x\"}", message);
+    char ulidBuffer[32] = {0};
+    struct ulid_generator generator = {0};
+
+    ulid_generator_init(&generator, ULID_SECURE);
+    ulid_generate(&generator, ulidBuffer);
+
+    char* postJSON = mprintf("{\"content\": \"%s\", \"nonce\": \"%i\"}", message, ulidBuffer);
     char* postURL = mprintf("https://api.revolt.chat/channels/%s/messages/", target);
     char* sessionHeader = mprintf("x-session-token: %s", client->token);
     char* useridHeader = mprintf("x-user-id: %s", client->userid);
