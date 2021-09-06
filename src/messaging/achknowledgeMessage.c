909f12fa5d5f51de4b0bfdf518f736e6ee26752b
Synth <synth9283@gmail.com>
2021-09-06 19:07:52 -0400

    feat: achknowledgeMessage.c

---
+++ src/messaging/achknowledgeMessage.c
@@ -0,0 +1,22 @@
+/*
+ * Source file for achknowledging messages on Revolt.
+*/
+
+#include "../deps/revolt.h"
+#include "../deps/cee-utils/json-actor.h"
+#include "../deps/json-utils/utils.h"
+
+int revoltPollMessageChanges(struct RevoltClient* client, const char* channel, const char* message) {
+    char* putURL = mprintf("https://api.revolt.chat/channels/%s/ack/%s", channel, message);
+    char* sessionHeader = mprintf("x-session-token: %s", client->token);
+    char* useridHeader = mprintf("x-user-id: %s", client->userid);
+
+    struct SizedBuffer response = putRequest(putURL, "", 2, sessionHeader, useridHeader);
+
+    free(response.string);
+    free(putURL);
+    free(sessionHeader);
+    free(useridHeader);
+
+    return 0;
+}
